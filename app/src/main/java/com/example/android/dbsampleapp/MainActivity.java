7e7a86c6d1214f09f01b3b7e5c054aac550e30bb
Manohar <kvmanohar@gmail.com>
2016-03-02 22:43:10 +0000

    Insert record code added

--- app/src/main/java/com/example/android/dbsampleapp/MainActivity.java
+++ app/src/main/java/com/example/android/dbsampleapp/MainActivity.java
@@ -1,23 +1,105 @@
 package com.example.android.dbsampleapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    public DBHelper dbHelper;
 
+    public EditText etID;
+    public EditText etFirstName;
+    public EditText etLastName;
+    public EditText etAddress;
+    public EditText etSalary;
+
+    public Button btnInsert;
+    public Button btnDelete;
+    public Button btnUpdate;
+    public Button btnLoad;
+
+    public ListView lvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper.OpenDB();
+        initialiseLayout();
+
     }
 
     public void initialiseLayout(){
+        etID = (EditText) findViewById(R.id.etID);
+        etFirstName = (EditText) findViewById(R.id.etFirstName);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etAddress = (EditText) findViewById(R.id.etAddress);
+        etSalary = (EditText) findViewById(R.id.etSalary);
 
+        btnInsert = (Button) findViewById(R.id.bt_insert);
+        btnDelete = (Button) findViewById(R.id.bt_delete);
+        btnUpdate = (Button) findViewById(R.id.bt_update);
+        btnLoad = (Button) findViewById(R.id.bt_load);
 
+        lvDisplay = (ListView) findViewById(R.id.lv_display);
+
+        btnInsert.setOnClickListener(dbButtonListener);
+        btnDelete.setOnClickListener(dbButtonListener);
+        btnUpdate.setOnClickListener(dbButtonListener);
+        btnLoad.setOnClickListener(dbButtonListener);
 
     }
 
+    public View.OnClickListener dbButtonListener = new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.bt_insert:
+                    long result = dbHelper.insertTableRow(-1,getValue(etFirstName),getValue(etLastName),getValue(etAddress),Double.valueOf(getValue(etSalary)));
+                    if (result == -1 ){
+                        Toast.makeText(getApplicationContext(),"Error inserting row",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(),"Record inserted sucessfully",Toast.LENGTH_SHORT).show();
+                    }
+
+                    break;
+                case R.id.bt_update:
+                    break;
+                case R.id.bt_delete:
+                    break;
+                case R.id.bt_load:
+                    break;
+            }
+        }
+    };
+
+
+//    private void populateListView(){
+//        Cursor cursor;
+//        cursor = dbHelper.getAllRecords();
+//
+//    }
+
+    private String getValue (EditText et){
+         return et.getText().toString().trim();
+
+    }
+
+    protected void onStart() {
+        super.onStart();
+        dbHelper.OpenDB();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        dbHelper.CloseDB();
+    }
 }
